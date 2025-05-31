6764f95f136d4c13ef8a1070227541d07b4e0d99
Ferglow <fernando.a.h@outlook.com>
2025-05-30 18:22:26 -0600

    Primer ejemplo de git  con proyecto de inventarios

---
+++ src/components/templates/HomeTemplate.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import { Btnsave, useAuthStore } from "../../index";
+export function HomeTemplate() {
+  const { signOut } = { useAuthStore };
+  return (
+    <Container>
+      <h1>Home template</h1>
+      <Btnsave titulo="Cerrar sesion" bgcolor="#fff" funcion={signOut} />
+    </Container>
+  );
+}
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  overflow: hidden;
+  background-color: ${(props) => props.theme.bgtotal};
+  color: ${({ theme }) => theme.text};
+  width: 100%;
+`;
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
